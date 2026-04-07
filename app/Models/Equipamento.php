50273ea86941c9c0ac46eff730b3be8375027270
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 <BernardoDiniz-1898>
2026-04-07 20:32:50 -0300

    model equipamentos

--- app/Models/Equipamento.php
+++ app/Models/Equipamento.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equipamentos extends Model
 {
-    //
+    protected $fillable = [
+        'nome',
+        'modelo',
+        'fabricante',
+        'numero_serie',
+        'localizacao',
+        'valor_estimado',
+        'status'
+    ];
+
+    protected $casts = [
+        'data_aquisicao' => 'date',
+    ];
 }
